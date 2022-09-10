656ff788dd31a7a0194df50ffc1e18000d1b016a
RojaAtashkar <103642013+RojaAtashkar@users.noreply.github.com>
2022-09-10 23:58:46 +0430

    change fxml versions

--- src/main/java/client/view/Menu.java
+++ src/main/java/client/view/Menu.java
@@ -43,7 +43,7 @@ public abstract class  Menu {
              circle.setFill(new ImagePattern(image));
         }
         catch (Exception e){
-            e.printStackTrace();
+
         }
     }
     public void setProfileImage(Circle circle, String address){
@@ -52,7 +52,7 @@ public abstract class  Menu {
             circle.setFill(new ImagePattern(image));
         }
         catch (Exception e){
-            e.printStackTrace();
+
         }
     }
 }
