55afdc4aede3d4ce835772ad857e1e9112d5cc27
RojaAtashkar <103642013+RojaAtashkar@users.noreply.github.com>
2022-09-12 14:20:45 +0430

    add read and write in controller.Controller add textFile dirs

--- src/main/java/client/controller/Controller.java
+++ src/main/java/client/controller/Controller.java
@@ -2,7 +2,12 @@ package client.controller;
 
 import server.database.UserDB;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public abstract class  Controller {
     protected UserDB userDB = UserDB.getInstance();
@@ -19,5 +24,38 @@ public abstract class  Controller {
             e.printStackTrace();
         }
         return null;
+    }
+    public String writeFile(String text, String fileName, String dirAddress) {
+        String fileAddress = dirAddress + fileName + ".txt";
+        File myObj = new File(fileAddress);
+        try {
+            if (myObj.createNewFile()) {
+                FileWriter myWriter = new FileWriter(fileAddress);
+                myWriter.write(text);
+                myWriter.close();
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error while creating or writing in to file in controller.Controller :" + fileAddress);
+           e.printStackTrace();
+        }
+        return fileAddress;
+    }
+    public String readFile(String fileAddress){
+        String text = "";
+        try {
+            File myObj = new File(fileAddress);
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                text += data + "\n";
+            }
+            myReader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Error while reading from file :" + fileAddress);
+            e.printStackTrace();
+        }
+        return text;
     }
 }
