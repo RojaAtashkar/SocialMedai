08f6196c2aeba9ac23c1ad3cf64f0318c1718fbf
RojaAtashkar <103642013+RojaAtashkar@users.noreply.github.com>
2022-09-12 15:27:43 +0430

    change dir of assets

--- src/main/java/client/view/ChatMenu.java
+++ src/main/java/client/view/ChatMenu.java
@@ -3,43 +3,21 @@ package client.view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 
-public class ChatMenu extends Menu{
-    private static Scene scene;
-    public ImageView timeLineImageView;
-    public ImageView exploreImageView;
-    public ImageView directImageView;
-    public ImageView groupImageView;
-    public ImageView newGroupImageView;
-    public ImageView profileImageView;
-    public ImageView SettingImageView;
-    public ImageView searchImageview;
-    public ImageView imoJImageView;
-    public Circle profileImage;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    public void initialize(){
-        setBarIcons(timeLineImageView,exploreImageView, directImageView, groupImageView, newGroupImageView, profileImageView, searchImageview);
-        setProfileImage(profileImage);
-    };
+public abstract class ChatMenu extends Menu  {
+    private static Scene scene;
+
+
     @Override
-    public void run(){  try {
-        FXMLLoader fxmlLoader = new FXMLLoader(DirectMenu.class.getResource("/fxml/chatMenu.fxml"));
-        scene = new Scene(fxmlLoader.load());
-        Menu.stage.setTitle("Directs");
-        Menu.stage.setScene(scene);
-        Menu.stage.show();
-    } catch (Exception e) {
-        System.out.println("Error while rendering chatMeu.fxml in direct Menu.");
-        System.out.println(e);
-        e.printStackTrace();
-    }};
-    public void newGroupMenu(ActionEvent event){};
-    public void runChatSetting(ActionEvent event){};
-    public void sendVideo(ActionEvent event){};
-    public void sendPicture(ActionEvent event){};
-    public void sendTextMessage(ActionEvent event){};
+    public  abstract void run();
+
 }
--- src/main/java/client/view/DirectMenu.java
+++ src/main/java/client/view/DirectMenu.java
@@ -3,11 +3,19 @@ package client.view;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 
-public class DirectMenu extends ChatMenu{
+public class DirectMenu extends ChatMenu {
     private static Scene scene;
     private static DirectMenu instance;
+    private Text userIDText;
+    private TextField userIDTextField;
     private static void setInstance(DirectMenu directMenu){
         DirectMenu.instance =directMenu;
     }
@@ -17,10 +25,34 @@ public class DirectMenu extends ChatMenu{
         return DirectMenu.instance;
     }
     public DirectMenu(){
-
     }
-    @FXML
-    public void initialize() {
+    protected void addFindUser(){
+        userIDTextField= new TextField();
+        Text text = new Text("Enter UserID:  ");
+        Button button = new Button();
+        button.setText("Find user");
+        HBox hBox = new HBox();
+        hBox.getChildren().addAll(text, userIDTextField);
+        userIDText = new Text("");
+        VBox vBox = new VBox();
+        vBox.getChildren().add(userIDText);
+        vBox.getChildren().addAll(userIDText, hBox);
+        optionsVbox.getChildren().add(vBox);
+    }
+
+    @Override
+    public void run() {
+         try {
+            FXMLLoader fxmlLoader = new FXMLLoader(DirectMenu.class.getResource("/fxml/chatMenu.fxml"));
+            scene = new Scene(fxmlLoader.load());
+            Menu.stage.setTitle("Directs");
+            Menu.stage.setScene(scene);
+            Menu.stage.show();
+        } catch (Exception e) {
+            System.out.println("Error while rendering chatMeu.fxml in direct Menu.");
+            System.out.println(e);
+            e.printStackTrace();
+        }
 
     }
 
@@ -42,9 +74,9 @@ public class DirectMenu extends ChatMenu{
     public void sendPicture(ActionEvent event) {
 
     }
-
     @Override
     public void sendTextMessage(ActionEvent event) {
 
     }
+
 }
